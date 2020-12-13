e039a4e2a41ee5e81ac6ce1329815c9cbd445a12
Anton Spitsyn <a.ch.clr@gmail.com>
2020-12-13 15:12:29 +0300

    refactoring

--- examples/main.rs
+++ examples/main.rs
@@ -1,119 +1,21 @@
-use async_trait::async_trait;
-use chrono::NaiveDateTime;
-use http_collector::collector::{HttpCollector, ResultsHandler};
-use http_collector::models::{Feed, FeedItem, FeedKind};
-use http_collector::result::Result;
-use std::sync::Arc;
-use tokio::sync::{mpsc, Mutex};
+fn main() {
+    use scraper::{Html, Selector};
 
-#[derive(Debug)]
-pub struct ForFeedItem {
-    pub title: Option<String>,
-    pub content: String,
-    pub pub_date: NaiveDateTime,
-    pub guid: String,
-    pub image_link: Option<String>,
-}
-
-impl From<&FeedItem> for ForFeedItem {
-    fn from(fi: &FeedItem) -> Self {
-        Self {
-            title: fi.title.clone(),
-            content: fi.content.to_string(),
-            pub_date: fi.pub_date,
-            guid: fi.guid.to_string(),
-            image_link: match &fi.image_link {
-                None => None,
-                Some(l) => Some(l.to_string()),
-            },
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct ForFeed {
-    pub image: Option<String>,
-    pub link: String,
-    pub kind: FeedKind,
-    pub name: String,
-    pub content: Vec<ForFeedItem>,
-}
-
-impl From<Feed> for ForFeed {
-    fn from(feed: Feed) -> Self {
-        Self {
-            image: feed.image,
-            link: feed.link,
-            kind: feed.kind,
-            name: feed.name,
-            content: feed.content.iter().map(ForFeedItem::from).collect(),
-        }
-    }
-}
-
-#[tokio::main]
-async fn main() {
-    let (sender, mut receiver) = mpsc::channel::<Result<(Feed, FeedKind, String)>>(2000);
-    let sender = Arc::new(Mutex::new(sender));
-
-    let proc = Handler::new(sender);
-    let collector = Arc::new(HttpCollector::new());
-    let crun = collector.clone();
-    let (mut sources_sender, sources_receiver) =
-        mpsc::channel::<Vec<(Option<FeedKind>, String)>>(2000);
-    tokio::spawn(async move { crun.run(sources_receiver, &proc).await });
-    tokio::spawn(async move {
-        loop {
-            sources_sender.send(get_sources()).await.unwrap();
-            sources_sender.send(get_sources()).await.unwrap();
-            sources_sender.send(get_sources()).await.unwrap();
-            sources_sender.send(get_sources()).await.unwrap();
-            sources_sender.send(get_sources()).await.unwrap();
-        }
-    });
-    let mut x: i32 = 7;
-    while x >= 0 {
-        println!("{:?}", receiver.recv().await);
-        x -= 1
-    }
-    let detected =
-        tokio::spawn(async move { collector.detect_feeds("https://google.com").await }).await;
-    println!("{:?}", detected);
-}
-
-pub struct Handler {
-    channel: Arc<Mutex<mpsc::Sender<Result<(Feed, FeedKind, String)>>>>,
-}
-
-impl Handler {
-    pub fn new(channel: Arc<Mutex<mpsc::Sender<Result<(Feed, FeedKind, String)>>>>) -> Self {
-        Self { channel }
-    }
-}
-
-#[async_trait]
-impl ResultsHandler for Handler {
-    async fn process(&self, result: Result<(&Feed, FeedKind, String)>) {
-        let update = match result {
-            Ok((updates, kind, link)) => Ok((updates.clone(), kind, link)),
-            Err(err) => Err(err),
-        };
-        let mut local = self.channel.lock().await;
-        local.send(update).await.unwrap();
-    }
-}
-
-fn get_sources() -> Vec<(Option<FeedKind>, String)> {
-    vec![
-        (
-            Some(FeedKind::RSS),
-            "https://habr.com/ru/rss/best/daily/?fl=ru".to_string(),
-        ),
-        (Some(FeedKind::WP), "https://google.com".to_string()),
-        (Some(FeedKind::Atom), "https://google.com".to_string()),
-        (
-            None,
-            "https://habr.com/ru/rss/best/daily/?fl=ru".to_string(),
-        ),
-    ]
+    let doc = Html::parse_fragment(
+        r#"
+        <html>
+        <head>
+        </head>
+        <body>
+        <div>
+        <a href="https://a.com/feeds/rss/"></a>
+        <a href="https://a.com/feeds/rss"></a>
+        <a href="/feeds/rss"></a>
+        </div>
+        </body>
+        </html>
+        "#,
+    );
+    let sel = Selector::parse(r#"a[href*="rss"]"#).unwrap();
+    println!("{:?}", doc.select(&sel).next());
 }
--- src/collector.rs
+++ src/collector.rs
@@ -27,11 +27,11 @@ pub struct CacheStub {}
 
 #[async_trait]
 impl Cache for CacheStub {
-    async fn get(&self, link_: &str) -> Option<FeedKind> {
+    async fn get(&self, _link: &str) -> Option<FeedKind> {
         None
     }
 
-    async fn set(&self, link_: &str, feed_kind_: &FeedKind) -> Result<()> {
+    async fn set(&self, _link: &str, _feed_kind: &FeedKind) -> Result<()> {
         Ok(())
     }
 }
@@ -40,6 +40,12 @@ impl Cache for CacheStub {
 pub struct HttpCollector<C: Cache> {
     client: Client,
     cache: C,
+}
+
+impl Default for HttpCollector<CacheStub> {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl HttpCollector<CacheStub> {
@@ -100,7 +106,7 @@ where
             _ => Ok(feeds
                 .iter()
                 .find(|f| f.kind == FeedKind::RSS)
-                .or(Some(feeds.first().unwrap()))
+                .or_else(|| Some(feeds.first().unwrap()))
                 .unwrap()
                 .clone()),
         }
@@ -111,17 +117,19 @@ where
             None => match self.cache.get(link).await {
                 None => {
                     let feed = self.scrape_unknown_feed_kind(link).await?;
-                    self.cache.set(link, &feed.kind);
+                    if let Err(e) = self.cache.set(link, &feed.kind).await {
+                        warn!("cache not set: {:?}", e);
+                    };
                     return Ok(feed);
                 }
                 Some(kind) => kind,
             },
-            Some(kind) => kind.clone(),
+            Some(kind) => kind,
         };
         let result = match kind {
             FeedKind::RSS => self.scrape_rss(link).await?,
             FeedKind::Atom => self.scrape_atom(link).await?,
-            FeedKind::WP => Err(Error::SourceNotSupported)?,
+            FeedKind::WP => return Err(Error::SourceNotSupported),
         };
         Ok(result)
     }
@@ -147,8 +155,9 @@ where
         parsed_doc: Html,
     ) -> Result<Vec<(String, FeedKind)>> {
         let page_scrape_url = Url::parse(link)?;
-        let mut for_check: Vec<(String, FeedKind)> = vec![];
+        let mut for_check = CheckLinks::new(&page_scrape_url);
 
+        // get link from specified links in <head></head>
         for (kind, selector) in vec![
             (
                 FeedKind::RSS,
@@ -160,17 +169,13 @@ where
             ),
         ] {
             for element in parsed_doc.select(&selector) {
-                element.value().attr("href").map(|href| {
-                    let mut link = String::new();
-                    if href.starts_with("/") {
-                        link.push_str(page_scrape_url.join(href).unwrap().as_str());
-                    } else {
-                        link.push_str(href);
-                    }
-                    for_check.push((link.to_string(), kind));
-                });
+                if let Some(href) = element.value().attr("href") {
+                    for_check.add_check_with_href(href, kind)
+                };
             }
         }
+
+        // get wordpress links from <head></head>
         let wp_selector = Selector::parse(r#"link[rel="https://api.w.org/"]"#).unwrap();
         if let Some(found_wp) = parsed_doc.select(&wp_selector).next() {
             if let Some(href) = found_wp.value().attr("href") {
@@ -181,36 +186,27 @@ where
                     page_scrape_url.join("wp/v2/posts")?.to_string(),
                     FeedKind::WP,
                 ));
-                match for_check
-                    .iter()
-                    .find(|(link, _kind)| link.ends_with("feed/"))
-                {
-                    None => {
-                        for_check.push((parsed_href.join("feed/")?.to_string(), FeedKind::RSS));
-                        for_check.push((page_scrape_url.join("feed/")?.to_string(), FeedKind::RSS));
-                    }
-                    Some(_) => {}
+
+                // if rss feeds not found yet, then we're going to check WP-provided RSS-feeds
+                let f = "feed/";
+                if !for_check.has_link_ends_with(f) {
+                    for_check.push((parsed_href.join(f)?.to_string(), FeedKind::RSS));
+                    for_check.push((page_scrape_url.join(f)?.to_string(), FeedKind::RSS));
                 };
             };
         };
         {
             if for_check.is_empty() {
-                let selector = Selector::parse(r#"a[href~="rss"]"#).unwrap();
+                let selector = Selector::parse(r#"a[href*="rss"]"#).unwrap();
                 let x = parsed_doc.select(&selector);
                 for element in x {
-                    element.value().attr("href").map(|href| {
-                        let mut link = String::new();
-                        if href.starts_with("/") {
-                            link.push_str(page_scrape_url.join(href).unwrap().as_str());
-                        } else {
-                            link.push_str(href);
-                        }
-                        for_check.push((link.to_string(), FeedKind::RSS));
-                    });
+                    if let Some(href) = element.value().attr("href") {
+                        for_check.add_check_with_href(href, FeedKind::RSS)
+                    };
                 }
             };
         }
-        Ok(for_check)
+        Ok(for_check.consume_results())
     }
 
     pub fn traverse_parsers(&self, link: &str, content: &str) -> Vec<Feed> {
@@ -273,6 +269,43 @@ where
     }
 }
 
+struct CheckLinks<'a> {
+    for_check: Vec<(String, FeedKind)>,
+    page_scrape_url: &'a Url,
+}
+
+impl<'a> CheckLinks<'a> {
+    pub fn consume_results(self) -> Vec<(String, FeedKind)> {
+        self.for_check
+    }
+
+    pub fn has_link_ends_with(&self, ends: &str) -> bool {
+        self.for_check.iter().any(|(link, _)| link.ends_with(ends))
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.for_check.is_empty()
+    }
+
+    pub fn new(page_scrape_url: &'a Url) -> Self {
+        Self {
+            page_scrape_url,
+            for_check: vec![],
+        }
+    }
+    pub fn push(&mut self, pair: (String, FeedKind)) {
+        self.for_check.push(pair)
+    }
+
+    pub fn add_check_with_href(&mut self, href: &str, kind: FeedKind) {
+        let link = if href.starts_with('/') {
+            self.page_scrape_url.join(href).unwrap().into_string()
+        } else {
+            href.to_string()
+        };
+        self.for_check.push((link, kind));
+    }
+}
 fn get_image(content: &str) -> Option<String> {
     let image_selector = Selector::parse("img").unwrap();
     let parsed_doc = Html::parse_document(content);
@@ -291,7 +324,7 @@ fn current_time() -> chrono::DateTime<chrono::FixedOffset> {
 
 fn get_feed_pub_date(pub_date: Option<&str>) -> chrono::NaiveDateTime {
     let pub_date = chrono::DateTime::parse_from_rfc2822(pub_date.unwrap_or_default())
-        .unwrap_or(current_time());
+        .unwrap_or_else(|_| current_time());
     pub_date.naive_utc()
 }
 
@@ -311,7 +344,10 @@ fn parse_atom_feed(link: &str, content: &str) -> Result<Feed> {
         feed_items.push(FeedItem {
             title: Some(item.title().to_string()),
             image_link: image,
-            pub_date: item.published().unwrap_or(item.updated()).naive_utc(),
+            pub_date: item
+                .published()
+                .unwrap_or_else(|| item.updated())
+                .naive_utc(),
             content: description.to_string(),
             guid: item.id.to_string(),
         })
@@ -331,7 +367,7 @@ fn parse_rss_feed(link: &str, content: &str) -> Result<Feed> {
     for item in channel.items() {
         let description = item
             .content()
-            .unwrap_or(item.description().unwrap_or_default());
+            .unwrap_or_else(|| item.description().unwrap_or_default());
         let mut guid = String::new();
         if item.guid().is_some() {
             guid.push_str(item.guid().unwrap().value())
@@ -350,7 +386,7 @@ fn parse_rss_feed(link: &str, content: &str) -> Result<Feed> {
         })
     }
     Ok(Feed {
-        image: channel.image().map_or(None, |i| Some(i.url().to_string())),
+        image: channel.image().map(|i| i.url().to_string()),
         link: link.to_string(),
         kind: FeedKind::RSS,
         name: channel.title().to_string(),
@@ -384,6 +420,8 @@ mod tests {
 
     #[test]
     fn test_detect_possible_feeds() {
+        let collector = HttpCollector::new();
+
         let content = Html::parse_fragment(
             r#"
         <html>
@@ -402,11 +440,10 @@ mod tests {
         </html>
         "#,
         );
-        let collector = HttpCollector::new();
         let detected = collector
-            .detect_possible_feeds("https://test.test", &content)
+            .detect_possible_feeds("https://test.test", content)
             .unwrap();
-        #[cfg(feature = "rss_predict")]
+
         assert_eq!(
             detected,
             vec![
@@ -422,29 +459,35 @@ mod tests {
                 ("https://test.test/wp/v2/posts".to_string(), FeedKind::WP),
                 ("https://wp-url.wp/feed/".to_string(), FeedKind::RSS),
                 ("https://test.test/feed/".to_string(), FeedKind::RSS),
-                ("https://a.com/feeds/rss/".to_string(), FeedKind::RSS),
-                ("https://a.com/feeds/rss".to_string(), FeedKind::RSS),
-                ("https://test.test/feeds/rss".to_string(), FeedKind::RSS),
             ]
         );
 
-        #[cfg(not(feature = "rss_predict"))]
+        let content = Html::parse_fragment(
+            r#"
+                <html>
+                <head>
+                </head>
+                <body>
+                <div>
+                <a href="https://a.com/feeds/rss/"></a>
+                <a href="https://a.com/feeds/rss"></a>
+                <a href="/feeds/rss"></a>
+                </div>
+                </body>
+                </html>
+                "#,
+        );
+
+        let detected = collector
+            .detect_possible_feeds("https://test.test", content)
+            .unwrap();
         assert_eq!(
             detected,
             vec![
-                (
-                    "https://test_detect_possible_feeds.rss".to_string(),
-                    FeedKind::RSS
-                ),
-                (
-                    "https://test_detect_possible_feeds.atom".to_string(),
-                    FeedKind::Atom
-                ),
-                ("https://wp-url.wp/wp/v2/posts".to_string(), FeedKind::WP),
-                ("https://test.test/wp/v2/posts".to_string(), FeedKind::WP),
-                ("https://wp-url.wp/feed/".to_string(), FeedKind::RSS),
-                ("https://test.test/feed/".to_string(), FeedKind::RSS),
+                ("https://a.com/feeds/rss/".to_string(), FeedKind::RSS),
+                ("https://a.com/feeds/rss".to_string(), FeedKind::RSS),
+                ("https://test.test/feeds/rss".to_string(), FeedKind::RSS)
             ]
-        )
+        );
     }
 }
